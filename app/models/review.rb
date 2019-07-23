3b0c59724865204a3de2043228bb81e425c066f2
Walter Cegarra <walter.cegarra@icloud.com>
2019-07-23 19:51:27 +0100

    Fixed validations for Reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :rating, inclusion: { in: [1, 2, 3, 4, 5], allow_nil: false }
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false }
   validates :restaurant, presence: true
+  validates :content, presence: true
 end
